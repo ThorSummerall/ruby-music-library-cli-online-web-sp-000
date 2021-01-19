1b6acbdb4efd8693b5322505d12f0fcaa862303c
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 01:35:52 +0000

    Automatically backed up by Learn

--- lib/concerns/Findable.rb
+++ lib/concerns/Findable.rb
@@ -1,3 +1,10 @@
 module Concerns::Findable
 
+  def self.find_by_name
+    def self.find_by_name(name)
+      Song.all.find do |song|
+        song if song.name == name
+      end
+    end
+  end
 end
