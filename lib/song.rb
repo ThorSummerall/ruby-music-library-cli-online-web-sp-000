57f060b3929aaf577fc152ac17d6809c62defc33
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 01:56:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,4 +63,8 @@ attr_accessor :name, :artist, :genre
     genre = Genre.find_or_create_by_name(genre_name)
     self.new(song_name, artist, genre)
   end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).save
+  end
 end
