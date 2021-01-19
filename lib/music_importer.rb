31d5b83686eb48a7040066a0b7437166c20fffbc
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 01:58:20 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,7 +9,9 @@ class MusicImporter
     Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
   end
 
-  def self.import
-
+  def import
+    self.files.each do |filename|
+      Song.create_from_filename(filename)
+    end
   end
 end
