7a1afe248e0828946f56150a7438a5e434809fe4
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 02:00:29 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -2,7 +2,7 @@ class MusicLibraryController
   attr_accessor :path
 
   def initialize(path = ".db/mp3s/")
-    importer = MusicIMporter.new(path)
+    importer = MusicImporter.new(path)
     import.import
   end
 end
