6e196e524f5760d84b356629a707e85adbebc033
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 02:25:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -84,6 +84,6 @@ class MusicLibraryController
     if input > 0 && input <= Song.all.length
       song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
       puts "Playing #{song.name} by #{song.artist.name}"
-    end 
+    end
   end
 end
