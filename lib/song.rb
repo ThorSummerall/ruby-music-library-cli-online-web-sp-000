8c8603d899c3628ba6e2ef5c473bf844f1aeae8c
Thor Summerall <thor.summerall@gmail.com>
2021-01-19 01:11:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ attr_accessor :name, :artist, :genre
 
 @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist unless artist == nil
+    self.genre = genre unless genre == nil
   end
 
   def self.all
